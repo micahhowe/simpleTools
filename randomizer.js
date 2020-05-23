e27c984bef78f437387d6544b1ecebdcc84e02e7
micahhowe <micahhowe182@gmail.com>
2020-05-22 21:39:09 -0600

    fixed lettering

--- randomizer.js
+++ randomizer.js
@@ -1,7 +1,6 @@
 function returnNum(){
-    //alert(Math.floor(Math.random() * 10)) 
     var entity = [];
     entity.push(Math.floor(Math.random() * 10))
-    document.getElementById("write").innerHTML = entity;
+    document.getElementById("write").innerHTML = (Math.floor(Math.random() * 10));
 }
 //I want this to keep adding a random number into the html element
